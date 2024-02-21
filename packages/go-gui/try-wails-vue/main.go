4f38fcfa9f36bb7efece49ff53e7aea5121f1d30
Henry.Huang <hhglory@outlook.com>
2024-02-21 19:32:28 +0800

    chore: update

--- packages/go-gui/try-wails-vue/main.go
+++ packages/go-gui/try-wails-vue/main.go
@@ -24,7 +24,7 @@ func main() {
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		LogLevel:         logger.DEBUG,
+		LogLevel:         logger.WARNING, //logger.DEBUG, // todo x: under dev mode
 		OnStartup:        app.startup,
 		OnBeforeClose:    app.beforeClose, // todo x: double check
 		Bind: []interface{}{
